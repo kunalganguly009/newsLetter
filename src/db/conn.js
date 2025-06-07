344846982330d130a53ed29b0ec4eacd615092ba
Kunal Ganguly <kunalganguly009@gmail.com>
2025-06-07 13:14:35 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,5 @@
 let mongoose = require("mongoose")
-
-
-
-// mongoose.connect("mongodb://0.0.0.0:27017/newsletterSignup", {
-mongoose.connect("mongodb+srv://newsletterUserName:newsletterPassword@cluster0.orz21ti.mongodb.net/", {
+mongoose.connect("mongodb+srv://newsLetterProject:newsLetterProjectPassword@cluster0.des80pz.mongodb.net/newsLetterProject?retryWrites=true&w=majority&appName=Cluster0", {
   useUnifiedTopology: true,
   useNewUrlParser: true
 }).then(()=>{
